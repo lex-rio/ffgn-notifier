6473bf57484f72c4d03e58718d4f4a89a5d269d2
alex <oleskandr.oryshchuk@globallogic.com>
2018-02-02 14:51:18 +0200

    keep only one last article

--- index.js
+++ index.js
@@ -7,26 +7,17 @@ const cheerio = require("cheerio");
 
 const bot = new TelegramBot(config.botToken, {polling: true});
 const users = require('./app/modules/users.js');
-const articles = require('./app/modules/articles.js');
 const baseUrl = 'http://ffgn.com.ua/';
+
+let lastAnnouncement = {link: '', games: []};
 
 users.save(config.admin, {teamsToWatch: ['globallogic']});
 
-bot.onText(/\/start/, (msg, match) => {
-    let user = users.save(msg.chat.id),
-        message = ["Последние анонсы:"];
-    articles.each((link, article) => message.push(link));
-    user.notify(bot, message.join("\n"));
-});
+bot.onText(/\/start/, msg => users.save(msg.chat.id).notify(bot, "Последний анонс: " + lastAnnouncement.link));
 
-bot.onText(/\/team (.+)/, (msg, match) => {
-    let user = users.getOne(msg.chat.id);
-    user.addTeam(match[1]);
-});
+bot.onText(/\/team (.+)/, (msg, match) => users.getOne(msg.chat.id).addTeam(match[1]));
 
-bot.onText(/\/test/, (msg, match) => {
-    bot.sendMessage(msg.chat.id, "I'm working here");
-});
+bot.onText(/\/test/, msg => bot.sendMessage(msg.chat.id, "I'm working here"));
 
 //in case of error, increase config.grabInterval to config.grabIntervalForError
 let loop = (config, request, cheerio, baseUrl) => {
@@ -40,26 +31,36 @@ let loop = (config, request, cheerio, baseUrl) => {
         const $ = cheerio.load(body),
             headers = $("#content .article_col:first-child article.post .entry-title a");
 
-        headers.each((index, el) => {
-            //if there is new article, notify each user
-            if (el.attribs.title.toLowerCase().indexOf('анонс') !== -1 && !articles.getOne(el.attribs.href)) {
-                articles.save(el.attribs.href, {});
-                downloadAnnouncement(el.attribs.href, games => {
-                    users.each((id, user) => {
-                        /** @var User user */
-                        let team = user.getTeam();
-                        if (team) {
-                            games.forEach(game => {
-                                if (game.string.indexOf(team) !== -1) {
-                                    user.notify(bot, game.time + ' ' + game.pair + ' ' + el.attribs.href);
-                                }
-                            });
-                        } else {
-                            user.notify(bot, el.attribs.href);
+        let lastAnnouncementArticle;
+
+        for (let i=0; i<headers.length; i++) {
+            if (headers[i].attribs.title.toLowerCase().indexOf('анонс') !== -1) {
+                lastAnnouncementArticle = headers[i];
+                break;
+            }
+        }
+
+        if (lastAnnouncementArticle.attribs.href === lastAnnouncement.link) {
+            return false;
+        }
+
+        downloadAnnouncement(lastAnnouncementArticle.attribs.href, games => {
+            lastAnnouncement.games = games;
+            lastAnnouncement.link = lastAnnouncementArticle.attribs.href;
+
+            users.each((id, user) => {
+                /** @var User user */
+                let team = user.getTeam();
+                if (team) {
+                    games.forEach(game => {
+                        if (game.string.indexOf(team) !== -1) {
+                            user.notify(bot, game.time + ' ' + game.pair + ' ' + lastAnnouncement.link);
                         }
                     });
-                });
-            }
+                } else {
+                    user.notify(bot, lastAnnouncement.link);
+                }
+            });
         });
     });
     setTimeout(loop, delay, config, request, cheerio, baseUrl);
